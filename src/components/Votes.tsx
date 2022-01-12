00fcea131cd88a20e5263469f2f17e2e90b29d52
Kat Gilbert <katgilbert99@gmail.com>
2022-01-11 23:35:37 -0500

    tweaked votes

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -18,22 +18,23 @@ export const Votes = () => {
     let strawberryAvg = strawberry / totalVotes * 100;
     strawberryAvg = parseFloat(strawberryAvg.toFixed(1))
 
+
     return (
         <div className="Votes">
             <h2>Vote Here</h2>
     <p>
-        <button onClick={() => setChocolate( chocolate += 1 )}>Chocolate</button>
-        <button onClick={() => setVanilla( vanilla += 1 )}>Vanilla</button>
-        <button onClick={() => setStrawberry( strawberry += 1 )}>Strawberry</button>
+        <button onClick={() => setChocolate(chocolate += 1)}>Chocolate</button>
+        <button onClick={() => setVanilla(vanilla += 1)}>Vanilla</button>
+        <button onClick={() => setStrawberry(strawberry += 1)}>Strawberry</button>
     </p>
-    <p>Chocolate: {chocolate} // {chocolateAvg}%</p>
-    <div className="chocolate" style={{ width: chocolateAvg + "%" }}></div>
+    <p>Chocolate: {chocolate} ({chocolateAvg}%)</p>
+    <div className={"chocolate"} style={{ width: chocolateAvg + "%" }}></div> 
 
-    <p>Vanilla: {vanilla} // {vanillaAvg}%</p>
-    <div className="vanilla" style={{ width: vanillaAvg + "%" }}></div>
+    <p>Vanilla: {vanilla} ({vanillaAvg}%)</p>
+    <div className={"vanilla"} style={{ width: vanillaAvg + "%" }}></div>
 
-    <p>Strawberry: {strawberry} // {strawberryAvg}%</p>
-    <div className="strawberry" style={{ width: strawberryAvg + "%" }}></div>
+    <p>Strawberry: {strawberry} ({strawberryAvg}%)</p>
+    <div className={"strawberry"} style={{ width: strawberryAvg + "%" }}></div>
         </div>
     )
 }
