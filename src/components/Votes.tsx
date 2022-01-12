38c299750272b941f6efb616d2fd5f2b77fb883c
Kat Gilbert <katgilbert99@gmail.com>
2022-01-12 11:13:30 -0500

    part 1 now done

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -18,7 +18,28 @@ export const Votes = () => {
     let strawberryAvg = strawberry / totalVotes * 100;
     strawberryAvg = parseFloat(strawberryAvg.toFixed(1)) || 0;
 
+    let strawGraph = "strawGraph";
+    let chocGraph = "chocGraph";
+    let vanGraph = "vanGraph";
+    let graphs = "graphs"
+    let hide = "hide";
+    let message = ""
 
+    if ( chocolate === 0) {
+        chocGraph = hide;
+        }
+    if (vanilla === 0) {
+        vanGraph = hide;
+        }
+    if (strawberry === 0) {
+        strawGraph = hide;
+    }
+    if (totalVotes === 0) {
+            graphs = hide;
+            message = "No votes yet";
+        }
+
+        
 return (
     <div className="Votes">
     <h2>Vote Here</h2>
@@ -27,20 +48,24 @@ return (
         <button onClick={() => setVanilla(vanilla += 1)}>Vanilla</button>
         <button onClick={() => setStrawberry(strawberry += 1)}>Strawberry</button>
     </p>
-        <div className="chocGraph">
+        {message}
+        <div className={graphs}>
+        <div className={chocGraph}>
         <p>Chocolate: {chocolate} ({chocolateAvg}%)</p>
         <div className={"chocolate"} style={{ width: chocolateAvg + "%" }}></div> 
         </div>
 
-        <div className="vanGraph">
+        <div className={vanGraph}>
         <p>Vanilla: {vanilla} ({vanillaAvg}%)</p>
         <div className={"vanilla"} style={{ width: vanillaAvg + "%" }}></div>
         </div>
 
-        <div className="strawGraph">
+        <div className={strawGraph}>
         <p>Strawberry: {strawberry} ({strawberryAvg}%)</p>
         <div className={"strawberry"} style={{ width: strawberryAvg + "%" }}></div>
         </div>
+        </div>
     </div>
+
     )
 }
